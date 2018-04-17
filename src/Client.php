b4a773e1c3f8a5b3d138e71dcf1a72d94300d6db
Jonathan Raftery <jonathan@jonathanraftery.com>
2018-04-17 07:21:17 -0500

    initial commit

---
+++ src/Client.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace jonathanraftery\Bullhorn\REST;
+use jonathanraftery\Bullhorn\REST\Authentication\Client as AuthClient;
+
+class Client
+{
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use jonathanraftery\Bullhorn\REST\Client as Client;
+
+final class ClientTest extends TestCase
+{
+}
